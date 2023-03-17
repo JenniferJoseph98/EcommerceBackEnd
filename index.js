b6588b18117918351e084f63a2f99f9f681d6791
JenniferJoseph98 <honeyjen1607@gmail.com>
2023-03-17 17:36:38 +0530

    CORS added

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const authRoutes = require("./routes/userRoutes");
 const productRoutes = require("./routes/productRoutes");
 require("dotenv").config();
+const cors = require("cors");
 mongoose.set("strictQuery", false);
 mongoose.connect(process.env.DB_URl, () => {
   console.log("Databases connected");
@@ -13,7 +14,7 @@ app.get("/", (req, res) => {
   res.send("Hi");
 });
 app.use(express.json());
-
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/api/users", authRoutes);
